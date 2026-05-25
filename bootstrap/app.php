c52d71d58cc63d1c67f8d9d41ffda44dfc080786
rickytreha <rickytreha@gmail.com>
2026-05-25 13:58:41 +0300

    ckeckout

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -22,7 +22,6 @@ return Application::configure(basePath: dirname(__DIR__))
         ]);
 
         $middleware->api(prepend: [
-            \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
             \App\Http\Middleware\SetLocale::class,
         ]);
     })
